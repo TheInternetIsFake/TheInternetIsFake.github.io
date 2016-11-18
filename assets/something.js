2cb1d012242a49a42ebc0d964540b65a2eb99f92
TheInternetIsFake <student@bcdm52560.local>
2016-11-18 10:34:32 -0500

    Add characters and such

--- assets/something.js
+++ assets/something.js
@@ -43,6 +43,53 @@ $( document ).ready(function() {
 					{$('body').css('color','black');};
 			}
 		});
+
+	var characters = {
+	count: 0,
+	appear: function(menu) {
+		// select a geometric char from array
+		// var menu = ['•','◊','∆'];
+
+		// select random integer from 0-2
+		var dart = Math.random();
+		dart = Math.floor(dart* menu.length );
+		var character = menu[dart];
+
+		var idName = 'char-' + this.count++;
+		// insert at the beginning of the body element
+		$('body').prepend('<span class="character" id="' + idName+ '">'  + character + '</span>');
+		// style it with css?
+	},
+	disappear: function() {
+		$('.character').hide();
+	},
+	move: function(destX, destY, count) {
+		var whichChar = Math.floor(Math.random()*characters.count);
+		alert(characters.count);
+		$('.character').animate(
+			{ top: destY, left: destX },
+		  	3000
+		);
+	}
+	// characters.newColor();
+	// change the characters to a random color, using hsl values
+	newColor: function() {
+		$('.character').css('color','hsl('+Math.random()*360+', '+Math.random()*100+'%, '+Math.random()*100+'%)');
+
+	},
+	// characters.newSize();
+	// change the character css to a random font-size between 50 and 400%
+	newSize: function() {
+		$('.character').css('font-size',Math.floor(Math.random()*350)+50+'%');
+	}
+}
+characters.appear(['•','◊','∆']);
+$('body').click( function(event) {
+	alert(event.pageX);
+	alert(event.pageY);
+	characters.move(event.pageX, event.pageY, characters.count)
+})
+
 // saturationValue = horizontal position*100/width
 // 
 // lightnessValue = vertical position*100/height
